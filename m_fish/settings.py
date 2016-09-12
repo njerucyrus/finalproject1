1dcafb372f5c51559fcae7635b0a992d4d4e9735
= <njerucyrus5@gmail.com>
2016-09-12 21:01:42 +0300

    ..

--- m_fish/settings.py
+++ m_fish/settings.py
@@ -82,23 +82,23 @@ WSGI_APPLICATION = 'm_fish.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.sqlite3',
-#          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#      }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'm_fish_db',
-        'USER': 'm_fish_user',
-        'PASSWORD': 'passwd123',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
 }
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'm_fish_db',
+#         'USER': 'm_fish_user',
+#         'PASSWORD': 'passwd123',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 
 # Password validation
